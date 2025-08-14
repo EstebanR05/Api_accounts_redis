20db36a3299e1fb4424b53a70342e4b4fec1ec6e
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2025-08-14 09:51:27 -0500

    feat: start project

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import axios from 'axios';
+import responseTime from 'response-time';
+
+const app = express();
+app.use(responseTime());
+
+app.get('/characters', async (_req, res) => {
+    const { data } = await axios.get('https://rickandmortyapi.com/api/character');
+    return res.json(data);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
